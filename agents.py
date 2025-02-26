9897797e688da93bdb6fa9ffeba7cc7e86ea1aa2
suskhanal <sus.khanalbt@gmail.com>
2025-02-26 06:41:43 -0800

    code files is ready with code skeleton

---
+++ agents.py
@@ -0,0 +1,8 @@
+import os
+from autogen import AssistantAgent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class ResearchAgent:
+    pass
---
+++ data_loader.py
@@ -0,0 +1,9 @@
+import requests
+import xml.etree.ElementTree as ET
+from scholarly import scholarly
+
+print("ok")
+
+
+class DataLoader:
+    pass
